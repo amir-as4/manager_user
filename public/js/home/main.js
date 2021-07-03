31b2737cc5c5c84cd5d61bcf3fb3760ea72a428d
ALI SOUDI <5484367-AMIR_ALI@users.noreply.gitlab.com>
2021-07-03 13:12:05 +0430

    changing in date 1400/04/12 script

--- public/js/home/main.js
+++ public/js/home/main.js
@@ -158,10 +158,62 @@ $(document).ready(function () {
 
     // favorites product
 
-    $("ul.gallery-options button.add-favorites").on("click",function () {
+    $("ul.gallery-options button.add-favorites").on("click", function () {
         $(this).toggleClass("favorites");
     });
 
     // favorites product
 
 });
+window.number_format = function (number, decimals, dec_point, thousands_point) {
+
+    if (number == null || !isFinite(number)) {
+        throw new TypeError("number is not valid");
+    }
+
+    if (!decimals) {
+        var len = number.toString().split('.').length;
+        decimals = len > 1 ? len : 0;
+    }
+
+    if (!dec_point) {
+        dec_point = '.';
+    }
+
+    if (!thousands_point) {
+        thousands_point = ',';
+    }
+    number = parseFloat(number).toFixed(decimals);
+
+    number = number.replace(".", dec_point);
+
+    var splitNum = number.split(dec_point);
+    splitNum[0] = splitNum[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousands_point);
+    number = splitNum.join(dec_point);
+
+    return number;
+}
+window.toPersianNum = function (num, dontTrim) {
+
+    var i = 0,
+
+        dontTrim = dontTrim || false,
+
+        num = dontTrim ? num.toString() : num.toString().trim(),
+        len = num.length,
+
+        res = '',
+        pos,
+
+        persianNumbers = typeof persianNumber == 'undefined' ?
+            ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'] :
+            persianNumbers;
+
+    for (; i < len; i++)
+        if ((pos = persianNumbers[num.charAt(i)]))
+            res += pos;
+        else
+            res += num.charAt(i);
+
+    return res;
+}
